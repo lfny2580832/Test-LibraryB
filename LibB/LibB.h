234c6230b2183f80e272ef6b97589d4be816aa69
niuyan <niuyan_i@didiglobal.com>
2020-12-23 14:20:36 +0800

    init

---
+++ LibB/LibB.h
@@ -0,0 +1,13 @@
+//
+//  LibB.h
+//  LibB
+//
+//  Created by didiniuyan on 2020/12/22.
+//  Copyright © 2020 didi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibB : NSObject
+- (void)printLib;
+@end
---
+++ LibB/LibB.m
@@ -0,0 +1,15 @@
+//
+//  LibB.m
+//  LibB
+//
+//  Created by didiniuyan on 2020/12/22.
+//  Copyright © 2020 didi. All rights reserved.
+//
+
+#import "LibB.h"
+
+@implementation LibB
+- (void)printLib{
+    NSLog(@"LibB在运行");
+}
+@end
